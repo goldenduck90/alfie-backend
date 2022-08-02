4cdb21ee4e3ce59ea9853d02052f41e2c1358c06
goldenduck90 <goldenduck90919@gmail.com>
2022-08-02 14:58:23 -0400

    setup logger & morgan

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
+import morganMiddleware from "./middleware/morgan"
 
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(morganMiddleware)
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
