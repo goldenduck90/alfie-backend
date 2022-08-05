0c259dfbad9fcb9a784da08ef5f7cec7462ea49b
goldenduck90 <goldenduck90919@gmail.com>
2022-08-05 02:50:08 -0400

    update

--- config/development.ts
+++ config/development.ts
@@ -1,4 +1,5 @@
 export default {
   dbUri: `mongodb+srv://joinalfie_dev_user:${process.env.DB_PASSWORD}@platform-staging-cluste.zn2qm3z.mongodb.net/?retryWrites=true&w=majority`,
-  baseUrl: "http://localhost:3000",
+  baseUrl:
+    "http://develop.platform.joinalfie.com.s3-website-us-east-1.amazonaws.com",
 }
