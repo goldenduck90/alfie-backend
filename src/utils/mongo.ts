85f8a9eddd4dedf00b13afe389afbd4bfd958335
goldenduck90 <goldenduck90919@gmail.com>
2022-11-16 11:51:16 -0700

    merge commits

--- src/utils/mongo.ts
+++ src/utils/mongo.ts
@@ -1,8 +1,9 @@
-import mongoose from "mongoose"
 import config from "config"
+import mongoose from "mongoose"
 
 export async function connectToMongo() {
   try {
+    console.log(config.get("dbUri"))
     await mongoose.connect(config.get("dbUri"))
     console.log("Connected to MongoDB")
   } catch (error) {
