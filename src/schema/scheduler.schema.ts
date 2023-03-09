f4f62d7af76e2ba7ef612210887da7e0c74ef34a
rm0nroe <rmonroe128@gmail.com>
2023-03-09 17:13:26 -0500

    breaking out bookings into its own function; cleaning up cal provider avail endpoint

--- src/schema/scheduler.schema.ts
+++ src/schema/scheduler.schema.ts
@@ -1,13 +1,92 @@
-import { Field, ObjectType } from "type-graphql"
+import { Field, ObjectType, registerEnumType } from "type-graphql"
+
+enum Status {
+  ACCEPTED = "ACCEPTED",
+  PENDING = "PENDING",
+  CANCELLED = "CANCELLED",
+  REJECTED = "REJECTED",
+}
+registerEnumType(Status, {
+  name: "Status",
+})
 
 @ObjectType()
-export class CalAvailability {
+export class Attendee {
+  @Field(() => String, { nullable: true })
+  name: string
+  @Field(() => String, { nullable: true })
+  email: string
+  @Field(() => String, { nullable: true })
+  timeZone: string
+  @Field(() => String, { nullable: true })
+  locale: string
+}
+
+@ObjectType()
+export class Availability {
   @Field(() => Number, { nullable: true })
-  eventLength: number
+  id: number
+  @Field(() => Number, { nullable: true })
+  eventTypeId: number
+  @Field(() => [Number], { nullable: true })
+  days: number[]
+  @Field(() => String, { nullable: true })
+  startTime: string
+  @Field(() => String, { nullable: true })
+  endTime: string
+}
+
+@ObjectType()
+export class Booking {
+  @Field(() => String)
+  title: string
+  @Field(() => String)
+  startTime: string
+  @Field(() => String)
+  endTime: string
+  @Field(() => String, { nullable: true })
+  recurringEventId: number
+  @Field(() => String, { nullable: true })
+  description: string
+  @Field(() => Status)
+  status: Status
+  @Field(() => String)
+  location: string
+  @Field(() => String, { nullable: true })
+  smsReminderNumber: string
+  @Field(() => Attendee)
+  attendees: Attendee[]
+}
+@ObjectType()
+export class EventType {
+  @Field(() => Number, { nullable: true })
+  length: number
   @Field(() => Number, { nullable: true })
   beforeEventBuffer: number
   @Field(() => Number, { nullable: true })
   afterEventBuffer: number
   @Field(() => Number, { nullable: true })
   minimumBookingNotice: number
+}
+@ObjectType()
+export class Schedule {
+  @Field(() => Number)
+  id: number
+  @Field(() => Number)
+  userId: number
+  @Field(() => String)
+  name: string
+  @Field(() => String, { nullable: true })
+  timeZone: string
+  @Field(() => [Availability], { nullable: true })
+  availability: Availability
+}
+@ObjectType()
+export class CalAvailability {
+  @Field(() => Schedule, { nullable: true })
+  schedule: Schedule
+  // @Field(() => [Booking], { nullable: true })
+  // bookings: Booking[]
+  @Field(() => EventType, { nullable: true })
+  eventType: EventType
 }
--- src/services/scheduler.service.ts
+++ src/services/scheduler.service.ts
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/node"
 import { ApolloError } from "apollo-server"
 import axios, { AxiosInstance } from "axios"
 import config from "config"
-import { CalAvailability } from "../schema/scheduler.schema"
+import { Booking, CalAvailability } from "../schema/scheduler.schema"
 import { ProviderModel } from "../schema/provider.schema"
 
 class SchedulerService {
@@ -20,8 +20,11 @@ class SchedulerService {
     })
   }
 
-  async getProviderAvailability(email: string, eventId = 1): Promise<any> {
-    let response
+  async getProviderAvailability(
+    email: string,
+    eventId = 1
+  ): Promise<CalAvailability> {
+    let response: CalAvailability
     const { notFound, calIdNotFound } = config.get("errors.provider") as any
     const provider = await ProviderModel.find().findByEmail(email).lean()
 
@@ -29,35 +32,83 @@ class SchedulerService {
       throw new ApolloError(notFound.message, notFound.code)
     }
 
-    if (!provider.calId) {
-      throw new ApolloError(calIdNotFound.message, calIdNotFound.code)
-    }
+    // if (!provider.calId) {
+    //   throw new ApolloError(calIdNotFound.message, calIdNotFound.code)
+    // }
 
     try {
-      const testProvider = { ...provider, calId: 1 }
-
-      const { data: scheduleData } = await this.axios.get(
-        `/schedules/${testProvider.calId}?apiKey=${process.env.CAL_API_KEY}`
-      )
-      const { data: eventTypeData } = await this.axios.get(
-        `/event-types/${eventId}?apiKey=${process.env.CAL_API_KEY}`
+      const { data: userData } = await this.axios.get(
+        `/v1/users/${provider.calId || 1}?apiKey=${process.env.CAL_API_KEY}`
       )
 
-      const { data: bookingsData } = await this.axios.get(
-        `/bookings/?apiKey=${process.env.CAL_API_KEY}`
+      // if (!userData?.defaultScheduleId) {
+      //   throw new ApolloError("Default schedule id not found", "NOT_FOUND")
+      // }
+
+      const schedulePromise = this.axios.get(
+        `/schedules/${userData?.defaultScheduleId || 1}?apiKey=${
+          process.env.CAL_API_KEY
+        }`
       )
+      const eventTypePromise = this.axios.get(
+        `/event-types/1?apiKey=${process.env.CAL_API_KEY}`
+      )
+      // const bookingsPromise = this.axios.get(
+      //   `/bookings/?apiKey=${process.env.CAL_API_KEY}`
+      // )
 
-      // // Provider schedule info
-      // const schedule = calSchedulesResponse.data.schedule
-      // // Event info
-      // const eventLength = eventTypeResponse.data.length
-      // const beforeEventBuffer = eventTypeResponse.data.event_type.beforeEventBuffer
-      // const afterEventBuffer = eventTypeResponse.data.event_type.afterEventBuffer
-      // const minimumBookingNotice = eventTypeResponse.data.event_type.minimumBookingNotice
+      const [
+        scheduleResponse,
+        eventTypeResponse,
+        // bookingsResponse
+      ] = await Promise.all([
+        schedulePromise,
+        eventTypePromise,
+        // bookingsPromise
+      ])
 
+      // provider schedule
+      const schedule = scheduleResponse.data.schedule
+      // event info
+      const length = eventTypeResponse.data.event_type.length
+      const beforeEventBuffer =
+        eventTypeResponse.data.event_type.beforeEventBuffer
+      const afterEventBuffer =
+        eventTypeResponse.data.event_type.afterEventBuffer
+      const minimumBookingNotice =
+        eventTypeResponse.data.event_type.minimumBookingNotice
+
+      // const bookings =
+      //   bookingsResponse.data.bookings?.length > 0
+      //     ? bookingsResponse.data.bookings.filter(
+      //         (booking: any) => booking.userId === userData.id
+      //       )
+      //     : []
+
+      response = {
+        schedule,
+        eventType: {
+          length,
+          beforeEventBuffer,
+          afterEventBuffer,
+          minimumBookingNotice,
+        },
+        // bookings,
+      }
       return response
     } catch (err) {
-      console.log("<ERROR>\n", err)
+      Sentry.captureException(err)
+      throw new ApolloError(err.message, "ERROR")
+    }
+  }
+
+  async getBookings(): Promise<Booking[]> {
+    try {
+      const { data } = await this.axios.get(
+        `/bookings/?apiKey=${process.env.CAL_API_KEY}`
+      )
+      return data.bookings
+    } catch (err) {
       Sentry.captureException(err)
       throw new ApolloError(err.message, "ERROR")
     }
