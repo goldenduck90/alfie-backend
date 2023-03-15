a8e98fd2840a66b29edd4727fb68cae201374b58
goldenduck90 <goldenduck90919@gmail.com>
2023-03-15 13:34:21 -0400

    update to use staging

--- config/development.ts
+++ config/development.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv"
+dotenv.config()
+
 export default {
   dbUri: `mongodb+srv://joinalfie_dev_user:${process.env.DB_PASSWORD}@platform-staging-cluste.zn2qm3z.mongodb.net/?retryWrites=true&w=majority`,
   baseUrl:
