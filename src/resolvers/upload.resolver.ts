203c0647ec19fd105c051ceec9573a23cc954ecd
rm0nroe <rmonroe128@gmail.com>
2023-03-20 19:47:30 -0400

    implementing akute file upload

--- src/resolvers/upload.resolver.ts
+++ src/resolvers/upload.resolver.ts
@@ -8,11 +8,17 @@ import {
   User,
 } from "../schema/user.schema"
 import S3Service from "../services/s3.service"
+import AkuteService from "../services/akute.service"
+import { AkuteDocument, DocUploadInput } from "../schema/akute.schema"
 
 @Resolver()
 export default class UploadResolver {
-  constructor(private s3Service: S3Service) {
+  constructor(
+    private akuteService: AkuteService,
+    private s3Service: S3Service
+  ) {
     this.s3Service = new S3Service()
+    this.akuteService = new AkuteService()
   }
 
   @Authorized([Role.Patient])
@@ -22,6 +28,12 @@ export default class UploadResolver {
     @Arg("requests", (_) => [SignedUrlRequest]) requests: SignedUrlRequest[]
   ) {
     return this.s3Service.requestSignedUrls(requests)
+  }
+
+  @Authorized([Role.Patient])
+  @Mutation(() => AkuteDocument)
+  uploadDocument(@Arg("input") input: DocUploadInput) {
+    return this.akuteService.uploadDocument(input)
   }
 
   @Authorized([Role.Patient])
