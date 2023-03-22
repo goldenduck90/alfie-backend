ac2c9c443686ccdad14b5db83a24d11afec2d894
goldenduck90 <goldenduck90919@gmail.com>
2023-03-21 23:13:52 -0400

    merge conflicts

--- src/utils/mongo.ts
+++ src/utils/mongo.ts
@@ -3,14 +3,9 @@ import mongoose from "mongoose"
 
 export async function connectToMongo() {
   try {
-<<<<<<< HEAD
-    console.log(config.get("dbUri"))
-    await mongoose.connect(`mongodb+srv://joinalfie_dev_user:${process.env.DB_PASSWORD}@platform-production-clu.wnd0f.mongodb.net/?retryWrites=true&w=majority`)
-=======
     const dbUri = config.get("dbUri") as string
     console.log(dbUri)
     await mongoose.connect(dbUri)
->>>>>>> develop
     console.log("Connected to MongoDB")
   } catch (error) {
     console.error(error)
