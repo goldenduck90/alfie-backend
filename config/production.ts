064e101dedff0df0166bbf60b10a89c5fa958404
goldenduck90 <goldenduck90919@gmail.com>
2023-04-06 22:50:06 -0400

    update ea url temporarily

--- config/production.ts
+++ config/production.ts
@@ -1,7 +1,8 @@
 export default {
   dbUri: `mongodb+srv://joinalfie_dev_user:${process.env.DB_PASSWORD}@platform-production-clu.wnd0f.mongodb.net/?retryWrites=true&w=majority`,
   baseUrl: "https://app.joinalfie.com",
-  easyAppointmentsApiUrl: "https://ea.joinalfie.com/index.php/api/v1",
+  easyAppointmentsApiUrl:
+    "http://production-ea-v2.us-east-1.elasticbeanstalk.com/index.php/api/v1",
   sendBirdApiUrl:
     "https://api-56D883B9-B30F-428B-8B7A-31184E513DF4.sendbird.com",
   defaultPriceId: "price_1KMv4hDOjl0X0gOqRIWXpGVz",
