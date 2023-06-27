207dde259a369005cacbbd9115e334d1ec505e01
John-Paul Simonis <simonis.johnpaul@gmail.com>
2023-06-26 17:04:46 -0700

    fix payer name lookup - progress

--- src/utils/lookupCPID.ts
+++ src/utils/lookupCPID.ts
@@ -2,15 +2,17 @@ import cpids from "./cpids.json"
 import stringSimilarity from "string-similarity-js"
 
 /** Looks up a CPID given a payer ID and optional primary name (payer name). Returns null if no matches were found. */
-const lookupCPID = (payerId: string, primaryName: string): string | null => {
+const lookupCPID = (payerId: string | null, primaryName: string, maxMatches: number = 12): string[] => {
   const payerIdMatches = cpids.filter(({ payer_id }) => payer_id == payerId)
 
-  if (payerIdMatches.length === 0) {
-    return null
+  if (payerIdMatches.length === 1) {
+    return [payerIdMatches[0].cpid]
   }
 
-  if (payerIdMatches.length === 1) {
-    return payerIdMatches[0].cpid
+  if (!payerId || payerIdMatches.length === 0) {
+    // lookup from payor name
+    const sortedMatches = sortAndScoreEntries(primaryName)
+    return sortedMatches.slice(0, maxMatches).map(({ cpid }) => cpid)
   }
 
   const similarities = payerIdMatches.map(({ primary_name }) =>
@@ -22,7 +24,37 @@ const lookupCPID = (payerId: string, primaryName: string): string | null => {
     0
   )
 
-  return payerIdMatches[highestIndex].cpid
+  return payerIdMatches
+}
+
+/**
+ * Returns the CPIDs table with an additional field: `matchScore`, used
+ * to sort by match strength on the primary_name field.
+ */
+function sortAndScoreEntries(search: string) {
+  const searchTerms = search.toLowerCase().split(/\s+/)
+  const sortedMatches = cpids
+    // search term matches
+    .map(entry => {
+      const nameTerms = entry.primary_name.toLowerCase().split(/\s+/)
+      const matchedNameTerms = nameTerms
+        .filter((nameTerm) => !searchTerms.some((searchTerm) => searchTerm === nameTerm))
+        .length
+      const unmatchedNameTerms = nameTerms.length - matchedNameTerms
+      const matchedSearchTerms = searchTerms
+        .filter((term) => entry.primary_name.toLowerCase().includes(term)).length
+      const unmatchedSearchTerms = searchTerms.length - matchedSearchTerms
+      const similarity = stringSimilarity(entry.primary_name, search)
+      return {
+        ...entry,
+        // (the number of matches (both ways) - the number of unmatched terms (both ways)) weighted by the similarity result
+        matchScore: (matchedNameTerms + matchedSearchTerms - unmatchedSearchTerms - unmatchedNameTerms) * similarity,
+      }
+    })
+    // sort by score
+    .sort((a, b) => b.matchScore - a.matchScore)
+
+  return sortedMatches
 }
 
 export default lookupCPID
