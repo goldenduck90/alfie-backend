71d703116c795c8c5b65fd9c1626f45a792193ee
goldenduck90 <goldenduck90919@gmail.com>
2023-11-15 17:24:30 -0500

    update

--- scripts/transferProviderPatientsSpecific.ts
+++ scripts/transferProviderPatientsSpecific.ts
@@ -1,17 +1,9 @@
 import UserService from "../src/services/user.service"
 import runShell from "./utils/runShell"
 
-const oldProviderID = "634f85f8ed227ada5a4c1414"
-const newProviderID = "649a800e7bf2521707603612"
-const patientIds = [
-  "6548fa0d931d182cc6c7d0bf",
-  "6543e54b931d182cc6c78fad",
-  "6542702d931d182cc6c77b11",
-  "65415516931d182cc6c76c95",
-  "653ff745931d182cc6c75ac8",
-  "653bd407931d182cc6c72dba",
-  "65527f3d8e3ba5e414b088ec",
-]
+const oldProviderID = "649a80737bf2521707603614"
+const newProviderID = "634f85f8ed227ada5a4c1414"
+const patientIds = ["654ae90f931d182cc6c7f4d6"]
 
 async function transferProviderPatientsSpecific() {
   // Validate script inputs
--- src/services/metriport.service.ts
+++ src/services/metriport.service.ts
@@ -195,6 +195,7 @@ class MetriportService {
           "MedicationAdministration",
           "MedicationRequest",
           "AllergyIntolerance",
+          "DiagnosticReport",
         ],
         sixMonthsAgo,
         today
